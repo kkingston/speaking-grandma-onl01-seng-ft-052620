d61b594d89c42a2369e09ad7b014f5414a681bc4
Kayla Kingston <kkingston09@gmail.com>
2020-05-28 11:00:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,8 @@ def speak_to_grandma(string)
     "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
+  elseif string == string.upcase
+    "NO, NOT SINCE 1938!"
   end
 end
 
