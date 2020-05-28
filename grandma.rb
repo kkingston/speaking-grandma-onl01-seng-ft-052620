4818d967b061aa319dc349faa2ab578fc61a6bcb
Kayla Kingston <kkingston09@gmail.com>
2020-05-28 10:09:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-if speak_to_grandma(phrase.uppercase)
+if speak_to_grandma
   return "HUH?! SPEAK UP, SONNY!"
 
   
