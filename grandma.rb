bd1e52370e281748a84678c2738498d6d4d40377
Kayla Kingston <kkingston09@gmail.com>
2020-05-28 10:49:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,8 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif string == 
+  elsif string == "Hi Nana, how are you"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 
